3c8f7f1da0450399e606f24ae0a9c3683760deff
mcjara <moises@trustportal.org>
2024-08-29 08:55:20 +0100

    Fixing issue on S3 Bucket Policy

--- main.tf
+++ main.tf
@@ -137,16 +137,16 @@ resource "aws_s3_bucket_policy" "logs_bucket-legacy" {
   "Version": "2012-10-17",
   "Statement": [
       {
-        Sid       = "RequireSSL",
-        Effect    = "Deny",
-        Principal = "*",
-        Action    = "s3:*",
-        Resource = [
+        "Sid"       = "RequireSSL",
+        "Effect"    = "Deny",
+        "Principal" = "*",
+        "Action"    = "s3:*",
+        "Resource" = [
           "arn:aws:s3:::${aws_s3_bucket.logs_bucket.id}/*",
           "arn:aws:s3:::${aws_s3_bucket.logs_bucket.id}"
         ],
-        Condition = {
-          Bool = {
+        "Condition" = {
+          "Bool" = {
             "aws:SecureTransport" = "false"
           }
         }
@@ -283,16 +283,16 @@ resource "aws_s3_bucket_policy" "logs_bucket-modern" {
   "Version": "2012-10-17",
   "Statement": [
       {
-        Sid       = "RequireSSL",
-        Effect    = "Deny",
-        Principal = "*",
-        Action    = "s3:*",
-        Resource = [
+        "Sid"       = "RequireSSL",
+        "Effect"    = "Deny",
+        "Principal" = "*",
+        "Action"    = "s3:*",
+        "Resource" = [
           "arn:aws:s3:::${aws_s3_bucket.logs_bucket.id}/*",
           "arn:aws:s3:::${aws_s3_bucket.logs_bucket.id}"
         ],
-        Condition = {
-          Bool = {
+        "Condition" = {
+          "Bool" = {
             "aws:SecureTransport" = "false"
           }
         }
